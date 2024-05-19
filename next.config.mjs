f70fa83afa9034bdf35874396e6c75201a11e204
JinXzAF <arianf2004@gmail.com>
2024-05-18 20:18:52 -0400

    added csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 import { createSecureHeaders } from "next-secure-headers";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { serverComponentsExternalPackages: ["mongoose"] },
@@ -29,6 +42,10 @@ const nextConfig = {
           {
             key: "X-XSS-Protection",
             value: "1; mode=block",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
           },
         ],
       },
