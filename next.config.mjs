86c37a09bc62bcbc0db2bf577af71ceb9028f73c
JinXzAF <arianf2004@gmail.com>
2024-05-18 20:02:59 -0400

    removed headers temporarily

--- next.config.mjs
+++ next.config.mjs
@@ -12,26 +12,6 @@ const nextConfig = {
       },
     ],
   },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: createSecureHeaders({
-          contentSecurityPolicy: {
-            directives: {
-              defaultSrc: ["'self'"],
-              styleSrc: ["'self'", "'unsafe-inline'"],
-              imgSrc: ["'self'"],
-              baseUri: "self",
-              formAction: "self",
-              frameAncestors: true,
-            },
-          },
-        }),
-      },
-    ];
-  },
 };
 
 export default nextConfig;
