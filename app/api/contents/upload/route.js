f9ad7064c49ebd7f184b0d7f16af077cfa554a09
JinXzAF <arianf2004@gmail.com>
2024-06-20 21:08:10 -0400

    changed env names

--- app/api/contents/upload/route.js
+++ app/api/contents/upload/route.js
@@ -13,8 +13,8 @@ import Course from "@/models/courseModel";
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY_ID,
   },
 });
 
