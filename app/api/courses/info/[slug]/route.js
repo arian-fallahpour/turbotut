74db9f81e2ce97fc2332407c60a69d51d34f8089
JinXzAF <arianf2004@gmail.com>
2024-06-22 12:44:45 -0400

    fixed timeout bug

--- app/api/courses/info/[slug]/route.js
+++ app/api/courses/info/[slug]/route.js
@@ -1,12 +1,15 @@
 import Course from "@/models/courseModel";
 import AppError from "@/utils/AppError";
 import { routeHandler } from "@/utils/authentication";
+import { connectDB } from "@/utils/database";
 import { NextResponse } from "next/server";
 
 export const GET = routeHandler(
   async function (req, { params }) {
     if (!params.slug)
       return new AppError("Please provide the slug of a course", 400);
+
+    await connectDB();
 
     // Find course and all of its unarchived chapters/lectures
     const course = await Course.findOne({ slug: params.slug }).populate({
