21f888d56f2b2f0337b701069fe63bf0d7e12de6
JinXzAF <arianf2004@gmail.com>
2024-06-27 21:01:23 -0400

    hides modal when deleting is successfull

--- components/pages/dashboard/DocumentModal/DeleteDocumentForm.js
+++ components/pages/dashboard/DocumentModal/DeleteDocumentForm.js
@@ -8,12 +8,7 @@ import Input from "@/components/Elements/Input/Input";
 import { GlobalErrorContext } from "@/store/error-context";
 import { startProgress, stopProgress, useRouter } from "next-nprogress-bar";
 
-const DeleteDocumentForm = ({
-  hideModal,
-  document,
-  collectionData,
-  cancelHandler,
-}) => {
+const DeleteDocumentForm = ({ hideModal, document, collectionData, cancelHandler }) => {
   const { setGlobalError } = useContext(GlobalErrorContext);
   const [disabled, setDisabled] = useState(true);
   const router = useRouter();
@@ -33,10 +28,7 @@ const DeleteDocumentForm = ({
       startProgress();
       setDisabled(true);
 
-      const res = await fetch(
-        `/api/${collectionData.name}/${document._id}/hard`,
-        { method: "DELETE" }
-      );
+      const res = await fetch(`/api/${collectionData.name}/${document._id}/hard`, { method: "DELETE" });
 
       stopProgress();
 
@@ -46,6 +38,7 @@ const DeleteDocumentForm = ({
         setDisabled(false);
       } else {
         router.back();
+        hideModal();
       }
     };
 
@@ -56,33 +49,22 @@ const DeleteDocumentForm = ({
     <Form className={classes.DocumentModalForm} onSubmit={onSubmitHandler}>
       <FormRow>
         <p className="paragraph color-red">
-          Warning: This will delete this {toSingular(collectionData.name)} and
-          all of its child documents.
+          Warning: This will delete this {toSingular(collectionData.name)} and all of its child documents.
         </p>
       </FormRow>
 
       <FormRow>
         <p className="paragraph">
-          Enter the name of this {toSingular(collectionData.name)} (
-          <i>{document.name}</i>) to confirm the deletion.
+          Enter the name of this {toSingular(collectionData.name)} (<i>{document.name}</i>) to confirm the deletion.
         </p>
       </FormRow>
 
       <FormRow>
-        <Input
-          label={`name of ${toSingular(collectionData.name)}`}
-          name=""
-          onChange={onChangeHandler}
-        />
+        <Input label={`name of ${toSingular(collectionData.name)}`} name="" onChange={onChangeHandler} />
       </FormRow>
 
       <FormRow className={classes.DocumentModalActions}>
-        <Button
-          styleName="glass"
-          variantName="red"
-          type="button"
-          onClick={cancelHandler}
-        >
+        <Button styleName="glass" variantName="red" type="button" onClick={cancelHandler}>
           cancel
         </Button>
         <Button isDisabled={disabled}>confirm</Button>
