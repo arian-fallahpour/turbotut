a0efa9f4cba943fa4b3550893f628d3884695e3b
JinXzAF <arianf2004@gmail.com>
2024-07-28 22:34:43 -0400

    fixed image sizes

--- components/pages/HomePage/CoursesSection/Course.js
+++ components/pages/HomePage/CoursesSection/Course.js
@@ -11,7 +11,7 @@ const Course = ({ course }) => {
         className={classes.CourseImage}
         src={course.image || "/images/courses/default.png"}
         alt={`${course.name} cover`}
-        sizes="(max-width: 400px) 90vw, (max-width: 600px) 55vw, (max-width: 900px) 37vw, (max-width: 1440px) 30vw, 20vw"
+        sizes="(max-width: 400px) 90vw, (max-width: 600px) 55vw, (max-width: 900px) 40vw, 35vw"
         fill
       />
       <span className={classes.CourseGradient} />
