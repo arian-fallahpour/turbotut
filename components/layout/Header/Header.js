cb850090eda9b31d854bcc4d3916dc50b0fefddf
JinXzAF <arianf2004@gmail.com>
2025-01-11 18:45:16 -0500

    added isLink attribute

--- components/layout/Header/Header.js
+++ components/layout/Header/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
           Easily learn entire high school subjects in only{" "}
           <span className="color-red underline-fancy">one sitting!</span>
         </p>
-        <Button styleName="shiny" href="/courses">
+        <Button styleName="shiny" href="/courses" isLink>
           Get Started
           <EastIcon />
         </Button>
